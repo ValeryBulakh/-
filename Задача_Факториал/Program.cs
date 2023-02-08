b397a730c5a402502d700560674c6eb290681625
Valery Bulakh <steepperec@gmail.com>
2023-02-08 23:32:48 +0300

    Задача_Факториал

---
+++ Задача_Факториал/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine()!);
+int res = 1;
+
+for (int i = 1; i <= n; i++)
+{
+    res = res * i;
+
+}
+
+Console.WriteLine($"={res}");
