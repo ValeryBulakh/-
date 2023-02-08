3b57ef2a7539887f7dd8e7db7acd81ae61a1212a
Valery Bulakh <steepperec@gmail.com>
2023-02-08 23:11:17 +0300

    Задача_СуммаЦифр

---
+++ Задача_СуммаЦифр/Program.cs
@@ -0,0 +1,14 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Clear();
+
+Console.WriteLine("Введите число ");
+int x = int.Parse(Console.ReadLine()!);
+int sum = 0;
+
+while (x > 0)
+{
+    sum = sum + x % 10;
+    x = x / 10;
+}
+Console.WriteLine(sum);
