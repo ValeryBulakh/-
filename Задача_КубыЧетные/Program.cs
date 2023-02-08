0e589be64200d0d2256b4ca3d266bc0a124fc685
Valery Bulakh <steepperec@gmail.com>
2023-02-08 23:46:29 +0300

    Задача_КубыЧетные

---
+++ Задача_КубыЧетные/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+
+Console.WriteLine("Введите число ");
+int x = int.Parse(Console.ReadLine()!);
+int res = 0;
+for (int i = 0; i < x; i++)
+{
+    res = i * i * i;
+
+if (res % 2 == 0) ;
+Console.WriteLine(res);
+}
