e468c848054f19cde02e15499a746727e04d61f7
Valery Bulakh <steepperec@gmail.com>
2023-02-15 01:57:20 +0300

    Задача_42

---
+++ Задача_42/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+
+string DecToBin(int dec)
+{
+string result = "";
+while (dec > 0)
+{
+result = (dec % 2).ToString() + result;
+dec /= 2;
+}
+return result;
+}
+
+Console.WriteLine("введите число");
+int ten = int.Parse(Console.ReadLine()!);
+Console.WriteLine(DecToBin(ten));
